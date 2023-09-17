7168042eedb85fc853c1d9083301dd406fa9602b
powerfulsaso <34785107+powerfulsaso@users.noreply.github.com>
2023-09-17 01:16:36 -0500

    Conexion con el API
    - Boton de likes responde con el API
    - Se agrega el boton de logout al perfil
    
    Se agrego codigo necesario para esta funcionalidad

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-function Navbar({ onLogoClick, onProfileClick }) {
+function Navbar({ onLogoClick, onProfileClick, loginOk }) {
 
     useEffect(() => {
         const logoBtn = document.getElementById("logoBtn");
@@ -8,10 +8,12 @@ function Navbar({ onLogoClick, onProfileClick }) {
             onLogoClick(false); //oculta el profile
         });
 
-        const profileBtn = document.getElementById("profileBtn");
-        profileBtn.addEventListener("click", function (e) {
-            onProfileClick(true);
-        });
+        if (loginOk) {
+            const profileBtn = document.getElementById("profileBtn");
+            profileBtn.addEventListener("click", function (e) {
+                onProfileClick(true);
+            });
+        }
     })
 
 
@@ -24,15 +26,19 @@ function Navbar({ onLogoClick, onProfileClick }) {
                     </svg>
                     three pics
                 </button>
-                <div className="d-flex">
-                    <button id="profileBtn" className="btn ">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                            <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                        </svg>
-                    </button>
+                {loginOk
+                    ?
+                    <div className="d-flex">
+                        <button id="profileBtn" className="btn ">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                            </svg>
+                        </button>
 
-                </div>
+                    </div>
+                    : ""
+                }
             </div>
         </nav >
     );
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,22 +1,32 @@
-//import "../css/profile.css";
+function Profile({ avatar, username, bio, onLogout }) {
 
-function Profile({ avatar, username, bio }) {
+    function onBtnLogout() {
+        console.log("logout");
+        onLogout();
+    }
+
     return (
+        <>
+            <div className="container mt-4" >
+                <div className="d-flex justify-content-center">
+                    <div className="col-md-4 text-center">
+                        <div className=" d-flex justify-content-center " style={{ margin: "20px" }}>
+                            <img src={avatar} alt="Foto de perfil" className="img-fluid rounded-circle" />
+                        </div>
+                        <h5><b>{username}</b></h5>
+                        {/* <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore sit magnam atque eos adipisci libero ipsum aspernatur eveniet sapiente similique consectetur, perspiciatis, cum, tempore temporibus provident dolorum nisi laboriosam enim!</p> */}
+                        <p>{bio}</p>
+                        <button className="btn btn-secondary mt-4"
+                            key={1}
+                            onClick={() => onBtnLogout()}
+                        >
+                            logout
+                        </button>
 
-        <div className="container mt-4" >
-            <div className="d-flex justify-content-center">
-                <div className="col-md-4 text-center">
-                    <div className=" d-flex justify-content-center " style={{ margin: "20px" }}>
-                        <img src={avatar} alt="Foto de perfil" className="img-fluid rounded-circle" />
                     </div>
-                    <h5><b>{username}</b></h5>
-                    {/* <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore sit magnam atque eos adipisci libero ipsum aspernatur eveniet sapiente similique consectetur, perspiciatis, cum, tempore temporibus provident dolorum nisi laboriosam enim!</p> */}
-                    <p>{bio}</p>
                 </div>
             </div>
-        </div>
-
-
+        </>
     )
 
 }
